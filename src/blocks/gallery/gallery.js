b6ef05b9de0613b223c17e754ed8841757b01f03
_tango <shurinskiy@mail.ru>
2024-06-19 19:22:21 +0500

    homepage before adaptive

--- src/blocks/gallery/gallery.js
+++ src/blocks/gallery/gallery.js
@@ -1,4 +1,16 @@
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+
 (() => {
 
+	document.querySelectorAll('.gallery__slider').forEach((item, i) => {
+		new Swiper(item, {
+			modules: [Navigation],
+			watchOverflow: true,
+			slidesPerView: 3,
+			spaceBetween: 26,
+			threshold: 10,
+		});
+	});
 
 })();
