672f50e58e52d1c6af7f7d0513a8cb239d998ccc
_tango <shurinskiy@mail.ru>
2024-06-19 20:48:37 +0500

    homepage before adaptive

--- src/blocks/gallery/gallery.js
+++ src/blocks/gallery/gallery.js
@@ -7,9 +7,33 @@ import { Navigation } from 'swiper/modules';
 		new Swiper(item, {
 			modules: [Navigation],
 			watchOverflow: true,
-			slidesPerView: 3,
-			spaceBetween: 26,
 			threshold: 10,
+			navigation: {
+				nextEl: `.gallery__navi_${i} .gallery__button_next`,
+				prevEl: `.gallery__navi_${i} .gallery__button_prev`,
+			},
+			on: {
+				beforeInit: function(el) {
+					item
+					.querySelector('.gallery__navi')
+					?.classList
+					.add(`gallery__navi_${i}`);
+				},
+			},
+			breakpoints: {
+				0: {
+					spaceBetween: 16,
+					slidesPerView: 1.2
+				},
+				480: {
+					spaceBetween: 16,
+					slidesPerView: 2
+				},
+				780: {
+					spaceBetween: 26,
+					slidesPerView: 3
+				}
+			}
 		});
 	});
 
