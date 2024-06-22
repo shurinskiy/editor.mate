f475433c1fbd66c922a698a4d9bce539fcd18960
_tango <shurinskiy@mail.ru>
2024-06-22 18:14:25 +0500

    detail fancybox

--- src/blocks/detail/detail.js
+++ src/blocks/detail/detail.js
@@ -1,4 +1,22 @@
+import { Fancybox } from "@fancyapps/ui";
+
 (() => {
 
+	Fancybox.bind('[data-fancybox]', {
+		Html: {
+			videoAutoplay: true,
+			videoRatio: 0,
+		},
+		Thumbs: {
+			type: 'classic'
+		},
+		Toolbar: {
+			display: {
+				left: ["infobar"],
+				middle: [],
+				right: ["iterateZoom", "close"],
+			},
+		},
+	});
 
 })();
